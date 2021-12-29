8aae6990ee2cd92bcf19a32150ca842fa4468618
Jorge Mendez Ortega <jorge.mendez.ortega@gmail.com>
2021-12-28 21:58:45 -0600

    rompiendo

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
+  const DEMO = 1;
   return <Component {...pageProps} />;
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 export default function Home() {
+  console.log(1)
+  const a = 1111;
   return (
     <>
       <h1>Hello Next.js23</h1>
     </>
-  );
+  )
 }
